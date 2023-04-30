fa410750e2e957c6c7ec94c38f366063f8435256
Arsenii <origingoy@gmail.com>
2023-04-30 17:11:48 +0300

    added groups/manager/users route

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from .views import MenuItemsView, MenuItemView
+from .views import MenuItemsView, MenuItemView, ManagersView
 
 urlpatterns = [
     path("", include('djoser.urls')),
     path("", include('djoser.urls.authtoken')),
+    
     path("menu-items", MenuItemsView.as_view()),
     path("menu-items/<int:pk>", MenuItemView.as_view()),
+    
+    path("groups/manager/users", ManagersView.as_view())
+    
 ]
