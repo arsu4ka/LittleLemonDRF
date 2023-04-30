0ae24f8a029a67b9c4eeee028c542aae464065a9
Arsenii <origingoy@gmail.com>
2023-04-30 13:30:16 +0300

    added MenuItemsView

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from .models import MenuItem
+from .serializer import MenuItemSerializer
 
-# Create your views here.
+class MenuItemsView(APIView):
+    
+    def get(self, request: Request):
+        menu_items = MenuItem.objects.all()
+        serialized_items = MenuItemSerializer(menu_items, many=True)
+        return Response(serialized_items.data)
