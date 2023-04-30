3db35dc579d513ddd8f654f1d3644b8520d879ae
Arsenii <origingoy@gmail.com>
2023-04-30 17:05:53 +0300

    added new views

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,12 +1,40 @@
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsDeliveryCrew, IsManager
 from .models import MenuItem
-from .serializer import MenuItemSerializer
+from django.contrib.auth.models import User
+from .serializer import MenuItemSerializer, UserSerializer
 
-class MenuItemsView(APIView):
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+        if self.request.method == "POST":
+            permission_classes += [IsManager]
+        return [permission() for permission in permission_classes]
+
+    
+class MenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+        if self.request.method != "GET":
+            permission_classes += [IsManager]
+        return [permission() for permission in permission_classes]
+    
+
+class ManagersView(APIView):
+    permission_classes = [IsManager]
     
     def get(self, request: Request):
-        menu_items = MenuItem.objects.all()
-        serialized_items = MenuItemSerializer(menu_items, many=True)
-        return Response(serialized_items.data)
+        managers = User.objects.filter(name="Manager")
+        managers_serializer = UserSerializer(managers, many=True)
+        return Response(managers_serializer.data)
