1d5113f84ca55e9c8becb9d1b24678185348d8e6
Arsenii <origingoy@gmail.com>
2023-04-30 17:05:44 +0300

    routes for the menu item/s views

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import MenuItemsView
+from .views import MenuItemsView, MenuItemView
 
 urlpatterns = [
     path("", include('djoser.urls')),
     path("", include('djoser.urls.authtoken')),
-    path("menu-items", MenuItemsView.as_view())
+    path("menu-items", MenuItemsView.as_view()),
+    path("menu-items/<int:pk>", MenuItemView.as_view()),
 ]
