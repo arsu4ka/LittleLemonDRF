d330b6db55bcf544b59c43c68ec373cce9d39500
Arsenii <origingoy@gmail.com>
2023-05-01 15:34:12 +0300

    added routes for managing managers and delivery

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import MenuItemsView, MenuItemView, ManagersView
+from .views import MenuItemsView, MenuItemView, ManagersView, DestroyManagerView, DeliveryCrewsView, DestroyDeliveryCrewView
 
 urlpatterns = [
     path("", include('djoser.urls')),
@@ -8,6 +8,9 @@ urlpatterns = [
     path("menu-items", MenuItemsView.as_view()),
     path("menu-items/<int:pk>", MenuItemView.as_view()),
     
-    path("groups/manager/users", ManagersView.as_view())
+    path("groups/manager/users", ManagersView.as_view()),
+    path("groups/manager/users/<int:pk>", DestroyManagerView.as_view()),
     
+    path("groups/delivery-crew/users", DeliveryCrewsView.as_view()),
+    path("groups/delivery-crew/users/<int:pk>", DestroyDeliveryCrewView.as_view()),
 ]
