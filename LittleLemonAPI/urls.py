82576ac855f27200d094afef3f0a59b5701695c3
Arsenii <origingoy@gmail.com>
2023-05-03 12:45:57 +0300

    updated urls

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path, include
-from .views import MenuItemsView, MenuItemView, ManagersView, DestroyManagerView, DeliveryCrewsView, DestroyDeliveryCrewView, CartListCreateDeleteView
+from . import views
+
 
 urlpatterns = [
     path("", include('djoser.urls')),
     path("", include('djoser.urls.authtoken')),
     
-    path("menu-items", MenuItemsView.as_view()),
-    path("menu-items/<int:pk>", MenuItemView.as_view()),
+    path("menu-items", views.MenuItemsView.as_view()),
+    path("menu-items/<int:pk>", views.MenuItemView.as_view()),
     
-    path("groups/manager/users", ManagersView.as_view()),
-    path("groups/manager/users/<int:pk>", DestroyManagerView.as_view()),
+    path("groups/manager/users", views.ManagersView.as_view()),
+    path("groups/manager/users/<int:pk>", views.DestroyManagerView.as_view()),
     
-    path("groups/delivery-crew/users", DeliveryCrewsView.as_view()),
-    path("groups/delivery-crew/users/<int:pk>", DestroyDeliveryCrewView.as_view()),
+    path("groups/delivery-crew/users", views.DeliveryCrewsView.as_view()),
+    path("groups/delivery-crew/users/<int:pk>", views.DestroyDeliveryCrewView.as_view()),
     
-    path("cart/menu-items", CartListCreateDeleteView.as_view()),
+    path("cart/menu-items", views.CartListCreateDestroyAPIView.as_view()),
     
 ]
