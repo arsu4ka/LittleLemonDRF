727e30712b2a9e49be1afe2d7ae4fe5c39f69755
Arsenii <origingoy@gmail.com>
2023-05-03 12:46:13 +0300

    added cart view, formatted file

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,16 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework import status
 from rest_framework import views
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
+from .mixins import SerializerByMethodMixin
 from .permissions import IsManager
 from .models import MenuItem, Cart
 from django.contrib.auth.models import User, Group
-from .serializers import MenuItemSerializer, UserSerializer, CartSerializer
+from .serializers import MenuItemSerializer, UserSerializer, CartSerializer, CartCreateSerializer
 
 MANAGER_GROUP = Group.objects.get(pk=1)
 DELIVERY_CREW = Group.objects.get(pk=2)
+
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
@@ -78,5 +81,29 @@ class DestroyDeliveryCrewView(views.APIView):
         return Response(UserSerializer(user).data, status.HTTP_200_OK)
 
 
-class CartListCreateDeleteView(generics.ListCreateAPIView):
-    raise NotImplemented()
+class CartListCreateDestroyAPIView(SerializerByMethodMixin, generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class_by_method = {
+        'get': CartSerializer,
+        'delete': CartSerializer,
+        'post': CartCreateSerializer
+    }
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+    def get(self, request):
+        instances = self.get_queryset()
+        serializer = self.get_serializer(instance=instances, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request):
+        instances = self.get_queryset()
+        instances.delete()
+        return Response(status=202)
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data, context={'user': self.request.user})
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+        return Response(self.serializer_class_by_method['get'](instance=instance).data, status=201)
