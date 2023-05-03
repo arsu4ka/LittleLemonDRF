cd9fc17e1a3d9e57682bb83d86877cedf8deeff6
Arsenii <origingoy@gmail.com>
2023-05-03 15:44:12 +0300

    temporary solution, need to fix this

--- LittleLemonAPI/permissions.py
+++ LittleLemonAPI/permissions.py
@@ -1,10 +1,13 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
+from LittleLemon.settings import MANAGER_GROUP_NAME, DELIVERY_CREW_GROUP_NAME
+
 
 class IsManager(BasePermission):
     def has_permission(self, request: Request, view):
-        return bool(request.user and request.user.groups.filter(name="Manager").exists())
+        
+        return bool(request.user and request.user.groups.filter(name=MANAGER_GROUP_NAME).exists())
 
 class IsDeliveryCrew(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.groups.filter(name="delivery").exists())
+        return bool(request.user and request.user.groups.filter(name=DELIVERY_CREW_GROUP_NAME).exists())
