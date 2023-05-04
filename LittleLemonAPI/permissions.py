038c988e112c5b98e7e98b94ed459c74094e6581
Arsenii <origingoy@gmail.com>
2023-05-04 15:07:30 +0300

    fixed permissions and added one new

--- LittleLemonAPI/permissions.py
+++ LittleLemonAPI/permissions.py
@@ -1,12 +1,16 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
-from LittleLemon.settings import MANAGER_GROUP_NAME, DELIVERY_CREW_GROUP_NAME
+from .constants import get_manager_group, get_delivery_crew_group
 
 
 class IsManager(BasePermission):
-    def has_permission(self, request: Request, view):
-        return bool(request.user and request.user.groups.filter(name=MANAGER_GROUP_NAME).exists())
+    def has_permission(self, request: Request, view = None):
+        return bool(request.user and request.user.groups.filter(pk=get_manager_group().pk).exists())
 
 class IsDeliveryCrew(BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.groups.filter(name=DELIVERY_CREW_GROUP_NAME).exists())
+    def has_permission(self, request, view = None):
+        return bool(request.user and request.user.groups.filter(pk=get_delivery_crew_group().pk).exists())
+    
+class IsWorker(BasePermission):
+    def has_permission(self, request, view = None):
+        return bool(request.user and (request.user.groups.filter(pk=get_delivery_crew_group().pk).exists() or request.user.groups.filter(pk=get_manager_group().pk).exists()))
