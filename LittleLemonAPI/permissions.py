06e835aaf5ba459f8ce0c28a61b51ab3eecb451b
Arsenii <origingoy@gmail.com>
2023-05-04 12:12:20 +0300

    removed unnecessary blank line

--- LittleLemonAPI/permissions.py
+++ LittleLemonAPI/permissions.py
@@ -5,7 +5,6 @@ from LittleLemon.settings import MANAGER_GROUP_NAME, DELIVERY_CREW_GROUP_NAME
 
 class IsManager(BasePermission):
     def has_permission(self, request: Request, view):
-        
         return bool(request.user and request.user.groups.filter(name=MANAGER_GROUP_NAME).exists())
 
 class IsDeliveryCrew(BasePermission):
